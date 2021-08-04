ef8a89d4713c50a223d2372c3dc46958dfa35a60
admin <matthewosu@gmail.com>
2021-08-03 18:59:39 -0700

    landing timeline game now refactored to have a mini version of the game. More styling needed but great progress made

--- src/landing-moduels/timeline.js
+++ src/landing-moduels/timeline.js
@@ -6,9 +6,206 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./header.js";
 
 export default function TimelineLanding() {
+    const [answered, setAnswered] = useState(false)
     useEffect(() =>{
         TimelineFunc()
     })
+    const histEvents = [
+      "First Web Page",
+      "World War II",
+      "Founding of Jamestown",
+    ];
+  
+    // Store listitems
+    const listItems = [];
+  
+    let counter = 0
+  
+    let dragStartIndex;
+  
+    useEffect(() => createList(), []);
+
+      function addEventListeners() {
+        const draggables = document.querySelectorAll(".draggable");
+        const dragListItems = document.querySelectorAll(".draggable-list li");
+    
+        draggables.forEach((draggable) => {
+          draggable.addEventListener("dragstart", dragStart);
+        });
+    
+        dragListItems.forEach((item) => {
+          item.addEventListener("dragover", dragOver);
+          item.addEventListener("drop", dragDrop);
+          item.addEventListener("dragenter", dragEnter);
+          item.addEventListener("dragleave", dragLeave);
+        });
+      }
+    function createList() {
+  
+      [...histEvents]
+  
+        //randomizes the list on load. Sort continues to randomize
+        .map((a) => ({ value: a, sort: Math.random() }))
+        // sort becomes basically the id
+        .sort((a, b) => a.sort - b.sort)
+        .map((a) => a.value)
+        //------
+  
+        .forEach((event, index) => {
+          if (counter <= histEvents.length){
+            counter += 1
+            console.log(histEvents.length, 'len')
+            console.log(counter, 'counter')
+          const listItem = document.createElement("li");
+          listItem.setAttribute("data-index", index);
+  
+          listItem.innerHTML = `
+             
+              <div class="draggable" draggable="true" >
+                <p class="event-name">${event}</p>
+  
+              </div>
+            `;
+  
+          listItems.push(listItem);
+          const draggable_list = document.getElementById("draggable-list");
+          draggable_list.appendChild(listItem);
+          }
+          else {
+  
+          }
+        });
+  
+      addEventListeners();
+    }
+
+    function dragStart() {
+      // console.log('Event: ', 'dragstart');
+      dragStartIndex = +this.closest("li").getAttribute("data-index");
+    }
+  
+    function dragEnter() {
+      // console.log('Event: ', 'dragenter');
+      this.classList.add("over");
+    }
+    function dragLeave() {
+      // console.log('Event: ', 'dragleave');
+      this.classList.remove("over");
+    }
+    function dragOver(e) {
+      // console.log('Event: ', 'dragover');
+      e.preventDefault();
+    }
+    function dragDrop() {
+      // console.log('Event: ', 'drop');
+      const dragEndIndex = +this.getAttribute("data-index");
+      swapItems(dragStartIndex, dragEndIndex);
+  
+      this.classList.remove("over");
+    }
+  
+    // Swap list items that are drag and drop
+    function swapItems(fromIndex, toIndex) {
+      const itemOne = listItems[fromIndex].querySelector(".draggable");
+      const itemTwo = listItems[toIndex].querySelector(".draggable");
+  
+      listItems[fromIndex].appendChild(itemTwo);
+      listItems[toIndex].appendChild(itemOne);
+    }
+
+
+
+    function handleClick2() {
+      if (answered == false) { 
+        checkOrder();
+        setAnswered(true)
+        document.getElementById('check').innerHTML = "Play again?"
+      }
+      else{
+        window.location.reload();
+      }
+    }
+    function Dates() {
+      const dates = [
+        "1991",
+        "1941",
+        "1606",
+      ];
+      return (
+        <>
+          <ul className="datesUl">
+            {dates.map((date) => (
+              <p>{date}</p>
+            ))}
+          </ul>
+        </>
+      );
+    }
+    function Markers() {
+      const [flagColor, setFlagColor] = useState('white')
+      const [flagBackColor, setBackColor] = useState('rgba(38, 97, 129, 0.767)')
+      let counter = 0
+      const markers = [
+        {id: "1"},
+        {id: "2"},
+        {id: "3"},
+      
+      ];
+      return (
+        <>
+          <ul className="markedUl">
+            {markers.map((num, index) => (
+             
+            
+              <p
+                id={num.id}
+                // id="TAH-marker-list-btn"
+                onClick={() => {
+                  if (counter %2 ){
+                    document.getElementById(num.id).style.backgroundColor =
+                    flagBackColor
+                    document.getElementById(num.id).style.color =
+                    flagColor
+                    counter += 1
+                  } else { 
+                  document.getElementById(num.id).style.backgroundColor =
+                  'white'
+                  document.getElementById(num.id).style.color =
+                  'black'
+                  counter += 1
+                  }
+                }}
+              >
+                x
+              </p>
+            ))}
+          </ul>
+        </>
+      );
+    }
+    
+
+
+  // Check the order of list items
+
+  function checkOrder() {
+
+    listItems.forEach((listItem, index) => {
+      const tevent = listItem.querySelector(".draggable").innerText.trim();
+      if (tevent !== histEvents[index]) {
+        listItem.classList.add("wrong");
+      } else {
+
+        listItem.classList.remove("wrong");
+        listItem.classList.add("right");
+        // disables game to stop incorrect right and wrong values on further drags
+        // document.getElementById('TAH-z-index-trick').style.zIndex = "0";
+
+      }
+    });
+  }
+
+    
     function TimelineFunc() {
         // Greensock Animations
         var tl = gsap.timeline({});
@@ -20,24 +217,21 @@ export default function TimelineLanding() {
         );
         gsap.to("#Timeline-Link", { delay: 3.5, duration: 1, opacity: 1 });
 
-
-        
         function Animation2() {
         tl2.to("#Timeline-Animation-Container", { opacity: 1 });
         tl2.to("#Timeline-Animation-Container", { delay: 2.2, duration: 1 });
-        tl2.to("#Timeline-Animation-Answers", { y: -42 });
+        tl2.to("#Timeline-Animation-Answers", { y: -65 });
         tl2.to("#Roe", { backgroundColor: "rgba(255, 0, 0, 0.5)" });
         tl2.to("#Nixon", { backgroundColor: "white" });
-        tl2.to("#Timeline-Animation-Answers", { delay: 1, y: -84 });
+        tl2.to("#Timeline-Animation-Answers", { delay: 1, y: -130 });
         tl2.to("#WW2", { backgroundColor: "rgba(255, 0, 0, 0.5)" });
         tl2.to("#Roe", { backgroundColor: "white" });
-        tl2.to("#Timeline-Animation-Answers", { delay: 1, y: -126 });
+        tl2.to("#Timeline-Animation-Answers", { delay: 1, y: -196 });
         tl2.to("#FirstWebPage", { backgroundColor: "rgba(123, 239, 178, 1)" });
         tl2.to("#WW2", { backgroundColor: "white" });
         tl2.to("#WW2", { delay: 2 });
         tl2.to("#Timeline-Animation-Container", { duration: 1, opacity: 0 });
         }
-    
       }
   return (
     <>
@@ -57,14 +251,49 @@ export default function TimelineLanding() {
             </p>
             <Router forceRefresh>
               <NavLink id="Timeline-Link" to="/timelinegame">
-                Visit Page
+                Visit Game
               </NavLink>
             </Router>
           </div>
 
+
+        <div className="TAH-app">
+
+
+
+          <p id="timelineLandingInst">DRAG AND DROP EVENTS TO THEIR DATES</p>
+          <p id="timelineLandingInst">click the x to flag your progress</p>
+          <div className="container-dates-events">
+            <Dates />
+            <Markers />
+            <ul className="draggable-list" id="draggable-list"></ul>
+          </div>
+          <div id="containerTlLandingBtn">
+            <button
+              className="landing-check-btn"
+              id="check"
+              onClick={() => handleClick2({}) }
+            >
+              <p>Check Answers</p>
+            </button>
+            </div>
+
+
+          <p id="TAH-mobile-notice">This program was designed as a Drag and Drop demo, which isnt mobile friendly.
+          <br></br>
+          Please view it on a computer :)
+          </p>
+      </div>
+
+
+
+
+
+
+{/* 
           <div id="Timeline-Animation-Container">
-            <div id="Timeline-Animation-Date">
-              <p>1991</p>
+            <div>
+              <p id="Timeline-Animation-Date">1991</p>
             </div>
             <div id="Timeline-Animation-Answers">
               <p id="Nixon">Nixon Resigns</p>
@@ -72,9 +301,13 @@ export default function TimelineLanding() {
               <p id="WW2">World War II</p>
               <p id="FirstWebPage">The First Web Page</p>
             </div>
-          </div>
+          </div> */}
 
       </div>
     </>
   );
 }
+
+
+
+
